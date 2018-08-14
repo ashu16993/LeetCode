3feadf5073e12bea878f22dfc1c6b07b7df6838b
ashu16993 <ashu1693@gmail.com>
2018-08-14 22:08:42 +0530

    Added a space optimised method

--- palindrome_number.py
+++ palindrome_number.py
@@ -17,8 +17,28 @@ class Solution:
         """
         if x<0:
             return False
-        rev_x = int(str(x)[::-1])
-        return (rev_x==x)
+        
+        
+        # Pythonic way
+        # rev_x = int(str(x)[::-1])
+        # return (rev_x==x)
+
+        # Alternate way
+        # By reversing just half of the number
+        # To check when we have reached half of the number.. 
+        # it should have lesser digits than its reversed part
+
+        # First remove the corner case of last digit being 0
+        if(x%10==0 and x!=0):
+            return False
+        
+        rev_x = 0
+        while(rev_x<x):
+            rev_x = rev_x*10+x%10
+            x=x//10
+        return (x==rev_x or x==rev_x//10)
+
+
 
 
 def main():
