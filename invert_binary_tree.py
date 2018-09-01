1a1af22fea70fbcd36dd9a5993274ff8941b6209
ashu16993 <ashu1693@gmail.com>
2018-09-01 23:44:17 +0530

    Binary tree problems

---
+++ invert_binary_tree.py
@@ -0,0 +1,45 @@
+'''
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+
+'''
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        # Use recursion
+
+        if root is not None:
+            root.left,root.right = root.right,root.left
+            root.left = self.invertTree(root.left)
+            root.right = self.invertTree(root.right)
+            return root
+        else:
+            return None
